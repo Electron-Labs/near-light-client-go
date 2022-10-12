cdd2539a17e9ccbb4e984a6eb689bb26700d6510
Jinank Jain <jinank94@gmail.com>
2022-10-12 07:49:14 +0000

    Add support for validating block for tx
    Signed-off-by: Jinank Jain <jinank94@gmail.com>

--- json.go
+++ json.go
@@ -27,6 +27,69 @@ type NearInnerLightView struct {
 	NextBpHash       string `json:"next_bp_hash"`
 }
 
+func (il NearInnerLightView) parse() (nearprimitive.BlockHeaderInnerLiteView, error) {
+	decoded_epoch_id := base58.Decode(il.EpochId)
+	epoch_id := &nearprimitive.CryptoHash{}
+	err := epoch_id.TryFromRaw(decoded_epoch_id)
+	if err != nil {
+		return nearprimitive.BlockHeaderInnerLiteView{}, fmt.Errorf("Failed to decode epoch id: %s", err)
+	}
+
+	decoded_next_epoch_id := base58.Decode(il.NextEpochId)
+	next_epoch_id := &nearprimitive.CryptoHash{}
+	err = next_epoch_id.TryFromRaw(decoded_next_epoch_id)
+	if err != nil {
+		return nearprimitive.BlockHeaderInnerLiteView{}, fmt.Errorf("Failed to decode next epoch id: %s", err)
+	}
+
+	decoded_prev_state := base58.Decode(il.PrevStateRoot)
+	prev_state := &nearprimitive.CryptoHash{}
+	err = prev_state.TryFromRaw(decoded_prev_state)
+	if err != nil {
+		return nearprimitive.BlockHeaderInnerLiteView{}, fmt.Errorf("Failed to decode prev state root: %s", err)
+	}
+
+	decoded_outcome_root := base58.Decode(il.OutcomeRoot)
+	outcome_root := &nearprimitive.CryptoHash{}
+	err = outcome_root.TryFromRaw(decoded_outcome_root)
+	if err != nil {
+		return nearprimitive.BlockHeaderInnerLiteView{}, fmt.Errorf("Failed to decode outcome root: %s", err)
+	}
+
+	decoded_next_bp_hash := base58.Decode(il.NextBpHash)
+	next_bp_hash := &nearprimitive.CryptoHash{}
+	err = next_bp_hash.TryFromRaw(decoded_next_bp_hash)
+	if err != nil {
+		return nearprimitive.BlockHeaderInnerLiteView{}, fmt.Errorf("Failed to decode next bp hash: %s", err)
+	}
+
+	decoded_block_merkle_root := base58.Decode(il.BlockMerkleRoot)
+	block_merkle_root := &nearprimitive.CryptoHash{}
+	err = block_merkle_root.TryFromRaw(decoded_block_merkle_root)
+	if err != nil {
+		return nearprimitive.BlockHeaderInnerLiteView{}, fmt.Errorf("Failed to decode block merkle root: %s", err)
+	}
+
+	timestamp_nanosec, err := strconv.ParseUint(il.TimestampNanosec, 10, 64)
+	if err != nil {
+		return nearprimitive.BlockHeaderInnerLiteView{}, fmt.Errorf("Failed to parse timestamp nanosec: %s", err)
+	}
+
+	bh := nearprimitive.BlockHeaderInnerLiteView{
+		Height:           nearprimitive.BlockHeight(il.Height),
+		EpochId:          *epoch_id,
+		NextEpochId:      *next_epoch_id,
+		PrevStateRoot:    *prev_state,
+		BlockMerkleRoot:  *block_merkle_root,
+		NextBpHash:       *next_bp_hash,
+		OutcomeRoot:      *outcome_root,
+		Timestamp:        timestamp_nanosec,
+		TimestampNanosec: timestamp_nanosec,
+	}
+
+	return bh, nil
+}
+
 type NearNextBps struct {
 	AccountId                   string `json:"account_id"`
 	PublicKey                   string `json:"public_key"`
@@ -53,6 +116,33 @@ type BlockHeaderLite struct {
 	InnerLite     NearInnerLightView `json:"inner_lite"`
 	InnerRestHash string             `json:"inner_rest_hash"`
 	PrevBlockHash string             `json:"prev_block_hash"`
+}
+
+func (b BlockHeaderLite) parse() (nearprimitive.LightClientBlockLiteView, error) {
+	inner_lite, err := b.InnerLite.parse()
+	if err != nil {
+		return nearprimitive.LightClientBlockLiteView{}, fmt.Errorf("Failed to parse inner lite: %s", err)
+	}
+
+	inner_rest_hash := &nearprimitive.CryptoHash{}
+	err = inner_rest_hash.TryFromRaw(base58.Decode(b.InnerRestHash))
+	if err != nil {
+		return nearprimitive.LightClientBlockLiteView{}, fmt.Errorf("Failed to parse inner rest hash: %s", err)
+	}
+
+	prev_block_hash := &nearprimitive.CryptoHash{}
+	err = prev_block_hash.TryFromRaw(base58.Decode(b.PrevBlockHash))
+	if err != nil {
+		return nearprimitive.LightClientBlockLiteView{}, fmt.Errorf("Failed to parse inner rest hash: %s", err)
+	}
+
+	lb := nearprimitive.LightClientBlockLiteView{
+		PrevBlockHash: *prev_block_hash,
+		InnerRestHash: *inner_rest_hash,
+		InnerLite:     inner_lite,
+	}
+
+	return lb, nil
 }
 
 type MetaData struct {
@@ -93,6 +183,124 @@ type TxResult struct {
 	OutcomeRootProof Proof           `json:"outcome_root_proof"`
 }
 
+type NearTxResult struct {
+	BlockHeaderLite  nearprimitive.LightClientBlockLiteView
+	BlockProof       nearprimitive.MerklePath
+	OutcomeProof     nearprimitive.OutcomeProof
+	OutcomeRootProof nearprimitive.MerklePath
+}
+
+func (tx TxResult) parse() (NearTxResult, error) {
+	near_tx_result := NearTxResult{}
+
+	lite_header, err := tx.BlockHeaderLite.parse()
+	if err != nil {
+		return near_tx_result, fmt.Errorf("Failed to parse lite header: %s", err)
+	}
+
+	block_proof, err := tx.BlockProof.parse()
+	if err != nil {
+		return near_tx_result, fmt.Errorf("Failed to parse block proof: %s", err)
+	}
+
+	outcome_proof, err := tx.OutcomeProof.parse()
+	if err != nil {
+		return near_tx_result, fmt.Errorf("Failed to parse outcome proof: %s", err)
+	}
+
+	outcome_root_proof, err := tx.OutcomeRootProof.parse()
+	if err != nil {
+		return near_tx_result, fmt.Errorf("Failed to parse outcome root proof: %s", err)
+	}
+
+	near_tx_result.BlockHeaderLite = lite_header
+	near_tx_result.BlockProof = block_proof
+	near_tx_result.OutcomeProof = outcome_proof
+	near_tx_result.OutcomeRootProof = outcome_root_proof
+
+	return near_tx_result, nil
+}
+
+func (bp Proof) parse() (nearprimitive.MerklePath, error) {
+	merklePath := nearprimitive.MerklePath{}
+
+	for i := 0; i < len(bp); i++ {
+		path_item_hash := &nearprimitive.CryptoHash{}
+		err := path_item_hash.TryFromRaw(base58.Decode(bp[i].Hash))
+		if err != nil {
+			return merklePath, fmt.Errorf("Failed to decode hash to base58: %s", err)
+		}
+		if bp[i].Direction == "Right" {
+			merklePath = append(merklePath, nearprimitive.MerklePathItem{Hash: nearprimitive.MerkleHash(*path_item_hash), Direction: nearprimitive.Right})
+		} else {
+			merklePath = append(merklePath, nearprimitive.MerklePathItem{Hash: nearprimitive.MerkleHash(*path_item_hash), Direction: nearprimitive.Left})
+		}
+	}
+	return merklePath, nil
+}
+
+func (op OutcomeProof) parse() (nearprimitive.OutcomeProof, error) {
+	receipt_ids := []nearprimitive.CryptoHash{}
+	single_receipt := nearprimitive.CryptoHash{}
+
+	for i := 0; i < len(op.Outcome.ReceiptIds); i++ {
+		err := single_receipt.TryFromRaw(base58.Decode(op.Outcome.ReceiptIds[i]))
+		receipt_ids = append(receipt_ids, single_receipt)
+		if err != nil {
+			return nearprimitive.OutcomeProof{}, fmt.Errorf("Failed to get receipt Id: %s", err)
+		}
+	}
+
+	token_burnt, _, _ := num.U128FromString(op.Outcome.TokensBurnt)
+
+	serialized_status, err := nearprimitive.IntoExecutionStatusView(op.Outcome.Status)
+	if err != nil {
+		return nearprimitive.OutcomeProof{}, fmt.Errorf("Failed to parse status: %s", err)
+	}
+
+	ser_status, err := borsh.Serialize(serialized_status)
+	if err != nil {
+		return nearprimitive.OutcomeProof{}, fmt.Errorf("Failed to serialize status: %s", err)
+	}
+
+	execution_outcome := nearprimitive.ExecutionOutcomeView{
+		Logs:        op.Outcome.Logs,
+		ReceiptIds:  receipt_ids,
+		GasBurnt:    nearprimitive.Gas(op.Outcome.GasBurnt),
+		TokensBurnt: token_burnt,
+		ExecutorId:  nearprimitive.AccountId(op.Outcome.ExecutorId),
+		Status:      ser_status,
+	}
+
+	id := &nearprimitive.CryptoHash{}
+	err = id.TryFromRaw(base58.Decode(op.Id))
+	if err != nil {
+		return nearprimitive.OutcomeProof{}, fmt.Errorf("Failed to decode Outcome Proof Id: %s", err)
+	}
+
+	block_hash := &nearprimitive.CryptoHash{}
+	err = block_hash.TryFromRaw(base58.Decode(op.BlockHash))
+	err = id.TryFromRaw(base58.Decode(op.Id))
+
+	if err != nil {
+		return nearprimitive.OutcomeProof{}, fmt.Errorf("Failed to Decode block Hash: %s", err)
+	}
+
+	proof, err := op.Proof.parse()
+	if err != nil {
+		return nearprimitive.OutcomeProof{}, fmt.Errorf("Failed to parse proof: %s", err)
+	}
+
+	outcome_proof := nearprimitive.OutcomeProof{
+		Proof:     proof,
+		BlockHash: *block_hash,
+		Id:        *id,
+		Outcome:   execution_outcome,
+	}
+
+	return outcome_proof, nil
+}
+
 type TxRpcResponse struct {
 	Id      string   `json:"id"`
 	Jsonrpc string   `json:"jsonrpc"`
@@ -116,6 +324,17 @@ func GetProof(bp Proof) []nearprimitive.MerklePathItem {
 		}
 	}
 	return merklePath
+}
+
+func GetTxProof(response string) (TxResult, error) {
+	bp := TxRpcResponse{}
+
+	err := json.Unmarshal([]byte(response), &bp)
+	if err != nil {
+		return bp.Result, fmt.Errorf("Failed to unmarshal RpcResponse: %s", err)
+	}
+
+	return bp.Result, nil
 }
 
 // GetOutcomeProof will give outcome proof and outcome root proof from Rpc json response from near node
